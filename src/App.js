454f47da566b0d4b47afa5d5381a61d6bd7e2674
Dianna Faulk <diannafaulk@gmail.com>
2018-09-22 18:35:04 -0400

    Implement dynamic page Title and Description components

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import "./App.css";
 import Preloader from "./components/Preloader";
 import CountyMap from "./components/CountyMap";
 import Histogram from "./components/Histogram";
+import { Title, Description } from "./components/Meta";
 import { loadAllData } from "./DataHandling";
 
 class App extends Component {
   state = {
     techSalaries: [],
     countyNames: [],
-    medianIncomes: []
+    medianIncomes: [],
+    filteredBy: {
+      USstate: "*",
+      year: "*",
+      jobTitle: "*"
+    }
   };
 
   componentWillMount() {
@@ -55,6 +61,13 @@ class App extends Component {
 
     return (
       <div className="App container">
+        <Title data={filteredSalaries} filteredBy={this.state.filteredBy} />
+        <Description
+          data={filteredSalaries}
+          allData={this.state.techSalaries}
+          medianIncomesByCounty={this.state.medianIncomesByCounty}
+          filteredBy={this.state.filteredBy}
+        />
         <svg width="1100" height="500">
           <CountyMap
             usTopoJson={this.state.usTopoJson}
